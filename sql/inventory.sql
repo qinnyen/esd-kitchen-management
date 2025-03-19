c82b13ef9ff1387a3df9a554bd2d698fa00212a8
Archer <archer.ngan.2023@smu.edu.sg>
2025-03-20 00:07:58 +0800

    Update inventory.sql
    reduced tomato and lettuce to be lower than threshold

--- sql/inventory.sql
+++ sql/inventory.sql
@@ -11,9 +11,9 @@ CREATE TABLE Inventory (
 
 INSERT INTO Inventory (IngredientName, QuantityAvailable, UnitOfMeasure, ExpiryDate, ReorderThreshold)
 VALUES 
-('Tomato', 50, 'kg', '2025-03-20', 10),
+('Tomato', 5, 'kg', '2025-03-20', 10),
 ('Cheese', 20, 'kg', '2025-03-18', 5),
 ('Dough', 30, 'kg', '2025-03-22', 8),
-('Lettuce', 15, 'kg', '2025-03-25', 5),
+('Lettuce', 1, 'kg', '2025-03-25', 5),
 ('Chicken', 10, 'kg', '2025-03-19', 3),
 ('Garlic Butter', 25, 'kg', '2025-03-21', 7);
